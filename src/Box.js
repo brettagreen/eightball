0937b7976dbadf467d2f4f58ea07f17faa2ffe24
Brett Green <brettalangreen@gmail.com>
2023-05-15 14:07:35 -0500

    clean up Box component

--- src/Box.js
+++ src/Box.js
@@ -1,8 +1,5 @@
-import './Box.css';
-
 const Box = (props) => {
-    console.log('props.color', props.color);
-    console.log('props.changed', props.changed);
+
     return (
         <div className="Box" style={{backgroundColor: props.color}}>
             {props.changed ? <p>changed!</p> : null}
